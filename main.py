6daaf16d217813a193b73e5aa3b467344c741f80
kmp145 <kmp145@users.noreply.github.com>
2017-01-02 21:11:21 +0800

    Added some debug code

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import gopigo
 import sys
 import time
 
-import atexit
-atexit.register(stop)
+#import atexit
+#atexit.register(stop)
 
 while 1:
     gopigo.led_on(0)
+    print ("LED turned off")
     time.sleep(0.5)
     gopigo.led_on(1)
+    print ("LED turned on")
     time.sleep(0.5)
 
